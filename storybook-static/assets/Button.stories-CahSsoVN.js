58d266bfcbec4eddcc30b1b26fa0b95641de82da
Krishna Babu <krishna.babu@e1-inc.com>
2024-10-03 16:46:02 +0530

    Storybook Publish

---
+++ storybook-static/assets/Button.stories-CahSsoVN.js
@@ -0,0 +1,22 @@
+import{f as b}from"./index-DcZGqPg1.js";import{B as y}from"./Button-BlFogS3e.js";import"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";import"./index-C1dzxQ9A.js";const x={title:"Example/Button",component:y,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{backgroundColor:{control:"color"}},args:{onClick:b()}},r={args:{primary:!0,label:"Button"}},a={args:{label:"Button",backgroundColor:"#416a37",size:"large"}},e={args:{size:"large",label:"Button"}},o={args:{size:"small",label:"Button"}};var s,t,n;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    primary: true,
+    label: 'Button'
+  }
+}`,...(n=(t=r.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};var l,c,m;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    label: 'Button',
+    backgroundColor: "#416a37",
+    size: "large"
+  }
+}`,...(m=(c=a.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};var u,p,i;e.parameters={...e.parameters,docs:{...(u=e.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    size: 'large',
+    label: 'Button'
+  }
+}`,...(i=(p=e.parameters)==null?void 0:p.docs)==null?void 0:i.source}}};var g,d,B;o.parameters={...o.parameters,docs:{...(g=o.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    size: 'small',
+    label: 'Button'
+  }
+}`,...(B=(d=o.parameters)==null?void 0:d.docs)==null?void 0:B.source}}};const _=["Primary","Secondary","Large","Small"];export{e as Large,r as Primary,a as Secondary,o as Small,_ as __namedExportsOrder,x as default};
---
+++ storybook-static/assets/Header.stories-ClAgrhbj.js
@@ -0,0 +1,7 @@
+import{f as o}from"./index-DcZGqPg1.js";import{H as p}from"./Header-B4WvEtSo.js";import"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";import"./index-C1dzxQ9A.js";import"./Button-BlFogS3e.js";const L={title:"Example/Header",component:p,tags:["autodocs"],parameters:{layout:"fullscreen"},args:{onLogin:o(),onLogout:o(),onCreateAccount:o()}},e={args:{user:{name:"Jack Chen"}}},r={};var a,s,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    user: {
+      "name": "Jack Chen"
+    }
+  }
+}`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};var n,c,m;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(m=(c=r.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};const H=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,H as __namedExportsOrder,L as default};
